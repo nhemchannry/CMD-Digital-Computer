59c06f77f911ce8ff11f659c683f4d30bbc1d081
monikaly13 <77734063+monikaly13@users.noreply.github.com>
2021-10-17 14:51:26 +0700

    new update

--- components/presentations/inputlogin.js
+++ components/presentations/inputlogin.js
@@ -1,8 +1,9 @@
-export default function InputLogin({label,type}){
+export default function InputLogin({label,type,style}){
     return(
         <div>
-            <label>{label}</label>
-         <input type={type} placeholder={label}/>
+         <input type={type} placeholder={label} style={{width: "100%", padding: "16px 20px",margin: "8px 0", display: "inline-block", border: "1px solid #ccc",
+borderRadius: "30px", boxSizing: "border-box"}}/>
         </div>
     )
 }
+
