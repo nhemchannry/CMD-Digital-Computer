712b814b8e1825b2dda2d667396f0698cbaf84cf
monikaly13 <77734063+monikaly13@users.noreply.github.com>
2021-10-31 13:50:52 +0700

    contact page email js is finished

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,8 @@ export default function About(){
   return(
     <div className={styles.aboutT}> 
       <h1>ABOUT US</h1>
-      <p>Your computer’s BIOS is the first thing that loads when you start your computer. It initializes your hardware before booting an operating system from your hard drive or another device. Many low-level system settings are only available in your BIOS. Modern computers predominantly ship with UEFI firmware, which is the successor to the traditional BIOS. But UEFI firmware and the BIOS are fairly similar. We’ve even seen modern PCs refer to their UEFI firmware settings screen as the “BIOS”.</p>
+      <h2>We provide the best service to serve 
+        you the best authentic product.</h2>
     </div>
   )
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { TextField } from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import emailjs from 'emailjs-com';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const useStyles= makeStyles((theme)=>({
     root:{
@@ -9,19 +14,46 @@ const useStyles= makeStyles((theme)=>({
     },
     form:{
         width: 400,
-        backgroundColor: 'green',
+        backgroundColor: 'white',
         margin: '0 auto',
-        padding: 40
-    }
-}))
+        padding: 25,
+        '& .input':{
+            width: '100%',
+            marginTop: 10
+        }
+    },
+    btn:{
+        marginTop: 15,
+        width: '100%'
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+      },
+      selectEmpty: {
+        marginTop: theme.spacing(2),
+      }
+}));
 export default function Contact(){
-    const classes = useStyles()
+    const classes = useStyles();
+
+    function handleSendEmail(e){
+        e.preventDefault();
+        console.log(e.target);
+        emailjs.sendForm("cmd-digital-server", "cmd-template", e.target, "user_lod2R8qXUXUch322dpXeT")
+        .then((res)=>{
+            console.log("Your message is sent successfully!");
+        }).catch((err)=>{
+            console.error(err)
+        })
+    }
     return(
         <div className={classes.root}>
-           <form className={classes.form}>
-           <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-           <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-           <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+           <form onSubmit={handleSendEmail} className={classes.form}>
+           <TextField name="fullName" required className="input" id="outlined-basic" label="Name" type="text" variant="outlined" />
+           <TextField name="email" required className="input" id="outlined-basic" label="Email" type="email" variant="outlined" />
+           <TextField name="message" required className="input" id="outlined-basic" label="Message" type="text" variant="outlined" />
+           <Button className={classes.btn} type="submit" variant="contained" color="primary ">Submit</Button>
            </form>
         </div>
     )
