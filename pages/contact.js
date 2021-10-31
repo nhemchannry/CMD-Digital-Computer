4d3dc815ac126f55fd4ad036bde11d59ed562a17
dalis123 <77906471+dalis123@users.noreply.github.com>
2021-10-31 15:52:52 +0700

    footer

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { makeStyles} from '@material-ui/core/styles'
 import Link from 'next/link'
-import Image from "next/image"
-import Box from "@material-ui/core/Box"
-
 
 const useStyles= makeStyles((theme)=>({
     header: {
@@ -37,34 +34,26 @@ const ContactPage=()=>{
     const classes= useStyles();
 
     return(
-      
         <div  className={ classes.header}>
             <h1 className={classes.h1}> CONTACT US </h1>
             <div className={classes.alltext}> 
-            <p className={ classes.cmd}> CMD-Digital-Computer( Phnom Penh)</p>
-            <p>Address: No. 360-362Eo, Kampuchea Krom Blvd.,
-             Sangkat Mittapheap,<br/> Khan 7 Makara, Phnom Penh, Cambodia</p>
-            <div className={ classes.numemail} >
-            <p>Sales: +855 99 555 729 | +855 15 503 999</p>
-            <p>Services: +855 93 52 61 71</p>
-            <p> 
-            <Link href ="/email"> Email: Sales@cmd-digital-computer.com</Link></p>
+                <p className={ classes.cmd}> CMD-Digital-Computer( Phnom Penh)</p>
+                <p>Address: No. 360-362Eo, Kampuchea Krom Blvd.,
+                Sangkat Mittapheap,<br/> Khan 7 Makara, Phnom Penh, Cambodia</p>
+                <div className={ classes.numemail} >
+                <p>Sales: +855 99 555 729 | +855 15 503 999</p>
+                <p>Services: +855 93 52 61 71</p>
+                <p> 
+                <Link href ="/email"> Email: Sales@cmd-digital-computer.com</Link></p>
+                </div>
+                
+                <div className={classes.businesshours } >
+                <p>Business Hours:</p>
+                <p>Sales       : 8: 00AM - 6: 30PM ( Monday to Sunday )</p>
+                <p>Services : 8: 00AM - 5: 30PM ( Monday to Sunday )</p>
+                </div>
             </div>
-            
-            <div className={classes.businesshours } >
-            <p>Business Hours:</p>
-            <p>Sales       : 8: 00AM - 6: 30PM ( Monday to Sunday )</p>
-            <p>Services : 8: 00AM - 5: 30PM ( Monday to Sunday )</p>
-            </div>
-        
-           
-            </div>
-            
         </div>
-       
-        
-        
-
     )
-}
+    }
 export default ContactPage;
