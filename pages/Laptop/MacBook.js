250d4f08b61f3b00b868871dcfc1fba97dde1675
SUMSOPHAVIN <77903950+SUMSOPHAVIN@users.noreply.github.com>
2021-11-03 18:04:21 +0700

    Update

--- pages/Laptop/MacBook.js
+++ pages/Laptop/MacBook.js
@@ -1,9 +1,30 @@
 import ProductCard from "../../components/presentations/ProductCard"
+import { Grid } from "@material-ui/core"
 export default function Mac()
 
 {
     return(
         <div>
+        <Grid container spacing ={0}>
+
+{/* 1*/}
+        <Grid item>
+        <ProductCard
+        Brand = "Mac"
+        Title = "MacBook Pro 2021 "
+        ImgSrc ="/Mac/MacBook Pro 2021.jpg"
+        OS ="OS: Big Sur"
+        CPU="CPU: 8th Gen Intel® Core™ i10"
+        VGA="VGA: Intel"
+        RAM="RAM: 8GB DDR4"
+        HDD=" HDD: 1TB M.2 "
+        SREEN=" SREEN: 15.3 Full HD (1920x1080), IPS-level gaming panel"
+        PRICE =" $4,949"
+        />
+        </Grid>
+
+{/* 2 */}
+        <Grid item>
         <ProductCard
         Brand = "Mac"
         Title = "MacBook Air 2020 "
@@ -14,10 +35,61 @@ export default function Mac()
         RAM="RAM: 8GB DDR4"
         HDD=" HDD: 1TB M.2 "
         SREEN=" SREEN: 15.3 Full HD (1920x1080), IPS-level gaming panel"
-        PRICE =" $1,949"
+        PRICE =" $2,949"
         />
+        </Grid>
+{/* 3 */}
+        <Grid item>
         <ProductCard
-        Brand = "Mac"
+        Brand = "MSI DELTA 15, AMD ADVANTAGE™ EDITION"
+        Title = "DELTA 15"
+        ImgSrc ="/MSI/DELTA 15.png"
+        OS ="OS:Windows 10 Pro"
+        CPU="CPU: AMD Ryzen™ 9 5900HX Mobile Processors"
+        VGA="VGA: AMD Radeon™ RX 6700M 10GB GDDR6 Graphics"
+        RAM="RAM: 16G DDR4"
+        HDD=" HDD: 1T SSD M2"
+        SREEN=" 15.6” Full HD (1920x1080), 240 Hz Refresh Rate, IPS-Level panel "
+        OTHER="2 fans and 11 heat pipes and Per-Key RGB gaming keyboard by SteelSeries "
+        PRICE ="$2099"
+        />
+        </Grid>
+{/* 4 */}
+        <Grid item>
+        <ProductCard
+        Brand = "MSI GE75 RAIDER"
+        Title = "GE75 RAIDER"
+        ImgSrc ="/MSI/GE75 RAIDER.jpg"
+        OS ="OS:Windows 10 Pro"
+        CPU="CPU: Latest 10th Gen Intel® Core™ i9 processor"
+        VGA="VGA: Latest GeForce® RTX SUPER™ 2080 graphics"
+        RAM="RAM: 16G DDR4"
+        HDD=" HDD: 1T SSD M2"
+        SREEN=" 17.3” Full HD (1920x1080) 240Hz IPS-level gaming display  "
+        OTHER="2 fans and 11 heat pipes and Per-Key RGB gaming keyboard by SteelSeries "
+        PRICE ="$819"
+        />
+        </Grid>
+{/* 5 */}
+        <Grid item>
+        <ProductCard
+        Brand = "MSI KATANA GF66"
+        Title = "KATANA GF66"
+        ImgSrc ="/MSI/KATANA GF66.jpg"
+        OS ="OS:Windows 10 Pro"
+        CPU="CPU: latest 11th Gen Intel® Core™ i7 processor"
+        VGA="VGA: GeForce® GTX 1650 Laptop GPU 4GB GDDR6"
+        RAM="RAM: 16G DDR4"
+        HDD=" HDD: 1T SSD M2"
+        SREEN=" 15.6” Full HD (1920x1080),144 Hz Refresh Rate, IPS-Level panel "
+        OTHER="2 fans and 11 heat pipes and Per-Key RGB gaming keyboard by SteelSeries "
+        PRICE ="$819"
+        />
+        </Grid>
+{/* 6 */}
+        <Grid item>
+        <ProductCard
+        Brand = "MSI GT76 TITAN"
         Title = "GT76 TITAN "
         ImgSrc ="/MSI/GT76 TITAN.jpg"
         OS ="OS:Windows 10 Pro"
@@ -30,8 +102,9 @@ export default function Mac()
         Mystic Light underglow and Per-Key RGB gaming keyboard by SteelSeries "
         PRICE ="$7999"
         />
-
-
+        </Grid>
+        </Grid>
+            
             
             
 
