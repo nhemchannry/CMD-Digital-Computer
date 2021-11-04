ef2ece20508eafd59e80893d3eb82cc9296d4805
SUMSOPHAVIN <77903950+SUMSOPHAVIN@users.noreply.github.com>
2021-11-04 23:43:30 +0700

    Update

--- pages/Laptop/ASUS.js
+++ pages/Laptop/ASUS.js
@@ -6,8 +6,23 @@ export default function Asus()
     return(
         <div>
         <Grid container spacing ={0}>
-
-{/* 1*/}
+{/* 1 */}
+        <Grid item>
+        <ProductCard
+        Brand = "ProArt Studiobook 16OLED"
+        Title = "ProArt Studiobook 16OLED"
+        ImgSrc ="/ASUS/ProArt Studiobook 16OLED.png"
+        OS ="OS: Windows 10 Pro"
+        CPU="CPU: Ryzen™ 5000 series"
+        VGA="VGA: GeForce RTX™ 3070"
+        RAM="RAM: 8G DDR4"
+        HDD=" HDD: 1TB M.2 "
+        SREEN=" SREEN: 15.6 Full HD (1920x1080), IPS-level gaming panel"
+        OTHER="For Creators"
+        PRICE ="$2099"
+        />
+        </Grid>
+{/* 2 */}
         <Grid item>
         <ProductCard
         Brand = "Asus Zenbook 14X"
@@ -23,8 +38,7 @@ export default function Asus()
         PRICE ="$999"
         />
         </Grid>
-
-{/* 2 */}
+{/* 3 */}
 
         <Grid item>
         <ProductCard
@@ -41,23 +55,7 @@ export default function Asus()
         PRICE ="$2999"
         />
         </Grid>
-{/* 3 */}
-        <Grid item>
-        <ProductCard
-        Brand = "Asus ROG G703GX-E5003T"
-        Title = "ROG G703GX-E5003T "
-        ImgSrc ="/ASUS/ROG G703.jpg"
-        OS ="OS: Windows 10 Pro"
-        CPU="CPU: 8th Gen Intel® Core™ i9"
-        VGA="VGA: GeForce RTX™ 2080"
-        RAM="RAM: 8G DDR4"
-        HDD=" HDD: 1TB M.2 "
-        SREEN=" SREEN: 17.3 Full HD (1920x1080), IPS-level gaming panel"
-        OTHER="RGB KEY"
-        PRICE ="$2099"
-        />
-        </Grid>
-{/* 4 */}
+{/* 4*/}
         <Grid item>
         <ProductCard
         Brand = "Asus Zenbook 14X"
@@ -70,10 +68,28 @@ export default function Asus()
         HDD=" HDD: 1T SSD M2"
         SREEN=" SREEN:  3840 x 2400 4K UHD resolution"
         OTHER="180˚ ErgoLift hinge for easy sharing "
-        PRICE ="$799"
+        PRICE ="$999"
         />
         </Grid>
 {/* 5 */}
+
+        <Grid item>
+        <ProductCard
+        Brand = "TUF Gaming FX505DY"
+        Title = "TUF Gaming FX505DY "
+        ImgSrc ="/ASUS/TUF Gaming FX505DY.png"
+        OS ="OS: Windows 10 Pro"
+        CPU="CPU:  11th Gen Intel® Core™ i9 CPU "
+        VGA="VGA: GeForce RTX™ 3080 GPU"
+        RAM="RAM: 8G DDR4"
+        HDD=" HDD: 512 M.2 "
+        SREEN=" SREEN: 15.6 Full HD (1920x1080), IPS-level gaming panel"
+        OTHER="RGB KEY"
+        PRICE ="$2099"
+        />
+        </Grid>
+
+{/* 6 */}
         <Grid item>
         <ProductCard
         Brand = "Asus ROG G703GX-E5003T"
@@ -87,22 +103,6 @@ export default function Asus()
         SREEN=" SREEN: 17.3 Full HD (1920x1080), IPS-level gaming panel"
         OTHER="RGB KEY"
         PRICE ="$2099"
-        />
-        </Grid>
-{/* 6 */}
-        <Grid item>
-        <ProductCard
-        Brand = "Asus Zenbook 14X"
-        Title = "Zenbook 14X OLD "
-        ImgSrc ="/ASUS/Zenbook 14X OLD.png"
-        OS ="OS:Windows 10 Pro"
-        CPU="CPU: 11th gen Intel® Core™ i7 processor"
-        VGA="VGA: Up to NVIDIA® GeForce MX450 discrete graphics"
-        RAM="RAM: 16G DDR4"
-        HDD=" HDD: 1T SSD M2"
-        SREEN=" SREEN:  3840 x 2400 4K UHD resolution"
-        OTHER="180˚ ErgoLift hinge for easy sharing "
-        PRICE ="$799"
         />
         </Grid>
         </Grid>
--- pages/Laptop/Dell.js
+++ pages/Laptop/Dell.js
@@ -19,7 +19,7 @@ export default function Dell()
         RAM="RAM: 32GB DDR4"
         HDD=" HDD: 1TB M.2 "
         SREEN=" SREEN: 15.3 Full HD (1920x1080), IPS-level gaming panel"
-        OTHER="RGB KEY"
+        OTHER="Dell"
         PRICE =" $1,949"
         />
         </Grid>
@@ -28,32 +28,32 @@ export default function Dell()
 
         <Grid item>
         <ProductCard
-        Brand = "Dell"
-        Title = "Latitude 9420 "
-        ImgSrc ="/Dell/Dell_Latitude__9420_3_view.0.jpg"
+        Brand = "Dell XPS 17"
+        Title = "Dell XPS 17"
+        ImgSrc ="/Dell/Dell XPS 17.JPG"
         OS ="OS: Windows 10 Pro"
-        CPU="CPU: 8th Gen Intel® Core™ i7"
-        VGA="VGA: GeForce RTX™ 1080"
-        RAM="RAM: 32GB DDR4"
-        HDD=" HDD: 1TB M.2 "
-        SREEN=" SREEN: 15.3 Full HD (1920x1080), IPS-level gaming panel"
-        OTHER="RGB KEY"
-        PRICE =" $1,949"
+        CPU="CPU: 10th Gen Intel® Core™ i7"
+        VGA="VGA: GeForce RTX™ 2060, 6G"
+        RAM="RAM: 16GB DDR4"
+        HDD=" HDD: 512GB M.2 "
+        SREEN=" SREEN: 17 Full HD (1920x1080), IPS-level gaming panel"
+        OTHER="Dell"
+        PRICE =" $1,799"
         />
         </Grid>
 {/* 3 */}
         <Grid item>
         <ProductCard
-        Brand = "Dell"
-        Title = "Latitude 9420 "
-        ImgSrc ="/Dell/Dell_Latitude__9420_3_view.0.jpg"
+        Brand = "Inspiron 15 3000"
+        Title = "Inspiron 15 3000"
+        ImgSrc ="/Dell/Inspiron 15 3000.JPG"
         OS ="OS: Windows 10 Pro"
         CPU="CPU: 8th Gen Intel® Core™ i7"
         VGA="VGA: GeForce RTX™ 1080"
         RAM="RAM: 32GB DDR4"
         HDD=" HDD: 1TB M.2 "
-        SREEN=" SREEN: 15.3 Full HD (1920x1080), IPS-level gaming panel"
-        OTHER="RGB KEY"
+        SREEN=" SREEN: 15.6 Full HD (1920x1080), IPS-level gaming panel"
+        OTHER="Dell"
         PRICE =" $1,949"
         />
         </Grid>
