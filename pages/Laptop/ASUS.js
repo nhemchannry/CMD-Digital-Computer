7c2c94eaaa22bebcefcd6d93f11c98bfd9bc2fff
SUMSOPHAVIN <77903950+SUMSOPHAVIN@users.noreply.github.com>
2021-11-04 23:07:28 +0700

    Update

--- pages/Laptop/ASUS.js
+++ pages/Laptop/ASUS.js
@@ -10,24 +10,6 @@ export default function Asus()
 {/* 1*/}
         <Grid item>
         <ProductCard
-        Brand = "Asus ROG G703GX-E5003T"
-        Title = "ROG G703GX-E5003T "
-        ImgSrc ="/ASUS/ROG G703.jpg"
-        OS ="OS: Windows 10 Pro"
-        CPU="CPU: 8th Gen Intel® Core™ i9"
-        VGA="VGA: GeForce RTX™ 2080"
-        RAM="RAM: 8G DDR4"
-        HDD=" HDD: 1TB M.2 "
-        SREEN=" SREEN: 17.3 Full HD (1920x1080), IPS-level gaming panel"
-        OTHER="RGB KEY"
-        PRICE ="$2099"
-        />
-        </Grid>
-
-{/* 2 */}
-
-        <Grid item>
-        <ProductCard
         Brand = "Asus Zenbook 14X"
         Title = "Zenbook 14X OLD "
         ImgSrc ="/ASUS/Zenbook 14X OLD.png"
@@ -38,7 +20,25 @@ export default function Asus()
         HDD=" HDD: 1T SSD M2"
         SREEN=" SREEN:  3840 x 2400 4K UHD resolution"
         OTHER="180˚ ErgoLift hinge for easy sharing "
-        PRICE ="$799"
+        PRICE ="$999"
+        />
+        </Grid>
+
+{/* 2 */}
+
+        <Grid item>
+        <ProductCard
+        Brand = "Asus ROG Zephyrus S17 GX703"
+        Title = "ROG Zephyrus S17 GX703 "
+        ImgSrc ="/ASUS/ROG Zephyrus S17 GX703.PNG"
+        OS ="OS: Windows 10 Pro"
+        CPU="CPU:  11th Gen Intel® Core™ i9 CPU "
+        VGA="VGA: GeForce RTX™ 3080 GPU"
+        RAM="RAM: 8G DDR4"
+        HDD=" HDD: 1TB M.2 "
+        SREEN=" SREEN: 17.3 Full HD (1920x1080), IPS-level gaming panel"
+        OTHER="RGB KEY"
+        PRICE ="$2999"
         />
         </Grid>
 {/* 3 */}
