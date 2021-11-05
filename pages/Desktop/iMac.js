1028917b74d5e7af0ec362e35c27249202ba0558
SUMSOPHAVIN <77903950+SUMSOPHAVIN@users.noreply.github.com>
2021-11-05 16:44:54 +0700

    Update

--- pages/Desktop/iMac.js
+++ pages/Desktop/iMac.js
@@ -6,109 +6,108 @@ export default function Mac_Desktop()
     return(
         <div>
         <Grid container spacing ={0}>
-
-{/* 1*/}
+{/* 1 */}
         <Grid item>
         <ProductCard
-        Brand = "Mac"
-        Title = "MacBook Pro 2021 "
-        ImgSrc ="/Mac/MacBook Pro 2021.jpg"
+        Brand = "iMac"
+        Title = "iMac 2021 "
+        ImgSrc ="/Mac/iMac/iMac Pink.jpg"
         OS ="OS: Big Sur, M1 Chip"
         CPU="CPU: 10-Core CPU"
         VGA="VGA: 16-Core GPU"
         RAM="RAM: 16GB Unified Memory"
         HDD=" HDD: 512GB SSD Storage"
-        SREEN=" SREEN: 16-inch Liquid Retina XDR display"
-        OTHER = "Magic Keyboard with Touch ID, 140W USB-C Power Adapter"
-        PRICE =" $4,949"
+        SREEN=" SREEN: 24-inch Liquid Retina XDR display"
+        OTHER = "Color : Pink"
+        PRICE =" $1,949"
         />
         </Grid>
-        
-
-
-
-
 {/* 2 */}
         <Grid item>
         <ProductCard
-        Brand = "Mac"
-        Title = "MacBook Air 2020 "
-        ImgSrc ="/Mac/MacBook Air 2020.jpg"
-        OS ="OS: Catalina, M1 Chip"
-        CPU="CPU: 8‑core CPU"
-        VGA="VGA: 8‑core GPU"
-        RAM="RAM: 8GB unified memory"
-        HDD=" HDD: 512GB SSD storage"
-        SREEN=" SREEN:  13-inch"
-        OTHER = "Magic Keyboard, Touch ID"
-        PRICE =" $1,249"
+        Brand = "iMac"
+        Title = "iMac 2021 "
+        ImgSrc ="/Mac/iMac/iMac Purple.jpg"
+        OS ="OS: Big Sur, M1 Chip"
+        CPU="CPU: 10-Core CPU"
+        VGA="VGA: 16-Core GPU"
+        RAM="RAM: 16GB Unified Memory"
+        HDD=" HDD: 512GB SSD Storage"
+        SREEN=" SREEN: 24-inch Liquid Retina XDR display"
+        OTHER = "Color : Purple"
+        PRICE =" $1,949"
         />
         </Grid>
 {/* 3 */}
         <Grid item>
         <ProductCard
-        Brand = "Mac"
-        Title = "MacBook Air 2020 "
-        ImgSrc ="/Mac/MacBook Air 2020, 256.jpg"
-        OS ="OS: Catalina, M1 Chip"
-        CPU="CPU: 8‑core CPU"
-        VGA="VGA: 7‑core GPU"
-        RAM="RAM: 8GB unified memory"
-        HDD=" HDD: 256GB SSD storage"
-        SREEN=" SREEN:  13-inch"
-        OTHER = "Magic Keyboard, Touch ID"
-        PRICE =" $999"
-        />
-        </Grid>
-{/* 4 */}
-        <Grid item>
-        <ProductCard
-        Brand = "Mac"
-        Title = "MacBook Air 2020 "
-        ImgSrc ="/Mac/MacBook Air 2020.jpg"
-        OS ="OS: Catalina, M1 Chip"
-        CPU="CPU: 8‑core CPU"
-        VGA="VGA: 8‑core GPU"
-        RAM="RAM: 8GB unified memory"
-        HDD=" HDD: 512GB SSD storage"
-        SREEN=" SREEN:  13-inch"
-        OTHER = "Magic Keyboard, Touch ID"
-        PRICE =" $1,249"
-        />
-        </Grid>
-{/* 5 */}
-        <Grid item>
-        <ProductCard
-        Brand = "Mac"
-        Title = "MacBook Air 2020 "
-        ImgSrc ="/Mac/MacBook Air 2020, 256.jpg"
-        OS ="OS: Catalina, M1 Chip"
-        CPU="CPU: 8‑core CPU"
-        VGA="VGA: 7‑core GPU"
-        RAM="RAM: 8GB unified memory"
-        HDD=" HDD: 256GB SSD storage"
-        SREEN=" SREEN:  13-inch"
-        OTHER = "Magic Keyboard, Touch ID"
-        PRICE =" $999"
-        />
-        </Grid>
-{/* 6 */}
-        <Grid item>
-        <ProductCard
-        Brand = "Mac"
-        Title = "MacBook Pro 2021 "
-        ImgSrc ="/Mac/MacBook Pro 2021.jpg"
+        Brand = "iMac"
+        Title = "iMac 2021 "
+        ImgSrc ="/Mac/iMac/iMac Blue.jpg"
         OS ="OS: Big Sur, M1 Chip"
         CPU="CPU: 10-Core CPU"
         VGA="VGA: 16-Core GPU"
         RAM="RAM: 16GB Unified Memory"
         HDD=" HDD: 512GB SSD Storage"
-        SREEN=" SREEN: 16-inch Liquid Retina XDR display"
-        OTHER = "Magic Keyboard with Touch ID, 140W USB-C Power Adapter"
-        PRICE =" $4,949"
+        SREEN=" SREEN: 24-inch Liquid Retina XDR display"
+        OTHER = "Color : Blue"
+        PRICE =" $1,949"
         />
         </Grid>
         
+{/* 4*/}
+        <Grid item>
+        <ProductCard
+        Brand = "iMac"
+        Title = "iMac 2021  "
+        ImgSrc ="/Mac/iMac/iMac Green.jpg"
+        OS ="OS: Big Sur, M1 Chip"
+        CPU="CPU: 10-Core CPU"
+        VGA="VGA: 16-Core GPU"
+        RAM="RAM: 16GB Unified Memory"
+        HDD=" HDD: 512GB SSD Storage"
+        SREEN=" SREEN: 24-inch Liquid Retina XDR display"
+        OTHER = "Color : Green"
+        PRICE =" $1,949"
+        />
+        </Grid>
+        
+
+
+
+
+{/* 5 */}
+        <Grid item>
+        <ProductCard
+        Brand = "iMac"
+        Title = "iMac 2021 "
+        ImgSrc ="/Mac/iMac/iMac Yellow.jpg"
+        OS ="OS: Big Sur, M1 Chip"
+        CPU="CPU: 10-Core CPU"
+        VGA="VGA: 16-Core GPU"
+        RAM="RAM: 16GB Unified Memory"
+        HDD=" HDD: 512GB SSD Storage"
+        SREEN=" SREEN: 24-inch Liquid Retina XDR display"
+        OTHER = "Color :Yellow"
+        PRICE =" $1,949"
+        />
+        </Grid>
+{/* 6 */}
+        <Grid item>
+        <ProductCard
+        Brand = "iMac"
+        Title = "iMac 2021 "
+        ImgSrc ="/Mac/iMac/iMac Orange.jpg"
+        OS ="OS: Big Sur, M1 Chip"
+        CPU="CPU: 10-Core CPU"
+        VGA="VGA: 16-Core GPU"
+        RAM="RAM: 16GB Unified Memory"
+        HDD=" HDD: 512GB SSD Storage"
+        SREEN=" SREEN: 24-inch Liquid Retina XDR display"
+        OTHER = "Color : Orange"
+        PRICE =" $1,949"
+        />
+        </Grid>
 
 
 
