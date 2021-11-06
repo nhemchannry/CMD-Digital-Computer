2b4e3c4ee7e883301825c7735b625fa25d0cadec
SUMSOPHAVIN <77903950+SUMSOPHAVIN@users.noreply.github.com>
2021-11-06 15:52:22 +0700

    Revert "Update"
    This reverts commit 69ba22d45537ecd42928307d6b4d46102be08e3d.

--- components/presentations/buttonacce.js
+++ components/presentations/buttonacce.js
@@ -4,12 +4,7 @@
   import Button from '@material-ui/core/Button';
   import Menu from '@material-ui/core/Menu';
   import MenuItem from '@material-ui/core/MenuItem';
-  import Link from 'next/link';
-  import MouseIcon from '@material-ui/icons/Mouse';
-  import KeyboardIcon from '@material-ui/icons/Keyboard';
-  import UsbIcon from '@material-ui/icons/Usb';
-  import LocalMallIcon from '@material-ui/icons/LocalMall';
-  import StorageIcon from '@material-ui/icons/Storage';
+  import link from 'next/link';
   
   export default function ACCESERY() {
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -34,22 +29,13 @@
           open={Boolean(anchorEl)}
           onClose={handleClose}
         >
-          <Link href = "/Accessory/Mouse">
-          <MenuItem onClick={handleClose}> <MouseIcon style={{marginRight: 25}}/> Mouse</MenuItem>
+          <Link href = "./Accessory/Mouse.js">
+          <MenuItem onClick={handleClose}>Mouse</MenuItem>
           </Link>
-          <Link href = "/Accessory/Keyboard">
-          <MenuItem onClick={handleClose}>< KeyboardIcon style={{marginRight: 25}}/> Keyboard</MenuItem>
-          </Link>
-          <Link href = "/Accessory/Flash">
-          <MenuItem onClick={handleClose}> <UsbIcon style={{marginRight: 25}}/> Flash</MenuItem>
-          </Link>
-          <Link href = "/Accessory/Bag">
-          <MenuItem onClick={handleClose}> <LocalMallIcon style={{marginRight: 25}}/> Bag</MenuItem>
-          </Link>
-          <Link href = "/Accessory/HDD_SSD">
-          <MenuItem onClick={handleClose}> <StorageIcon style={{marginRight: 25}}/> HDD_SSD</MenuItem>
-          </Link>
-
+          <MenuItem onClick={handleClose}>Keyboard</MenuItem>
+          <MenuItem onClick={handleClose}>Flash</MenuItem>
+          <MenuItem onClick={handleClose}>Bag</MenuItem>
+          <MenuItem onClick={handleClose}>HDD_SSD</MenuItem>
         </Menu>
       </div>
     );
--- components/presentations/buttondesk.js
+++ components/presentations/buttondesk.js
@@ -1,9 +1,8 @@
-import DesktopMacIcon from '@material-ui/icons/DesktopMac';
+
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import Link from 'next/link';
 
 export default function Desktop() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -28,21 +27,11 @@ export default function Desktop() {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <Link href = "/Desktop/MSI_Desktop">
-        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> MSI</MenuItem>
-        </Link>
-        <Link href = "/Desktop/ASUS_Desktop">
-        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> ASUS</MenuItem>
-        </Link>
-        <Link href = "/Desktop/Dell_Desktop">
-        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> Dell</MenuItem>
-        </Link>
-        <Link href = "/Desktop/Lenovo_Desktop">
-        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> Lenovo</MenuItem>
-        </Link>
-        <Link href = "/Desktop/iMac">
-        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> Mac</MenuItem>
-        </Link>
+        <MenuItem onClick={handleClose}>MSI</MenuItem>
+        <MenuItem onClick={handleClose}>ASUS</MenuItem>
+        <MenuItem onClick={handleClose}>Dell</MenuItem>
+        <MenuItem onClick={handleClose}>Lenovo</MenuItem>
+        <MenuItem onClick={handleClose}>Mac</MenuItem>
       </Menu>
     </div>
   );
