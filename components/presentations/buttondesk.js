95e34382062e92e06c815f30aba05dd71a14f98c
SUMSOPHAVIN <77903950+SUMSOPHAVIN@users.noreply.github.com>
2021-11-06 12:34:08 +0700

    Update

--- components/presentations/buttondesk.js
+++ components/presentations/buttondesk.js
@@ -1,114 +1,49 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import EditIcon from '@mui/icons-material/Edit';
-import Divider from '@mui/material/Divider';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import ImportantDevicesIcon from '@material-ui/icons/ImportantDevices';
-const StyledMenu = styled((props) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color:
-      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
-        ),
-      },
-    },
-  },
-}));
+import DesktopMacIcon from '@material-ui/icons/DesktopMac';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Link from 'next/link';
 
 export default function Desktop() {
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
 
   return (
     <div>
-      <Button
-        id="demo-customized-button"
-        aria-controls="demo-customized-menu"
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        variant="contained"
-        disableElevation
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
-      >
-        DESKTOP
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Desktop
       </Button>
-      <StyledMenu
-        id="demo-customized-menu"
-        MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
-        }}
+      <Menu
+        id="simple-menu"
         anchorEl={anchorEl}
-        open={open}
+        keepMounted
+        open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose} disableRipple>
-          <ImportantDevicesIcon/>
-          APPLE
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <ImportantDevicesIcon/>
-          ASUS
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <ImportantDevicesIcon/>
-          DELL
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <ImportantDevicesIcon/>
-          LENOVO
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <ImportantDevicesIcon/>
-          ACER
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <ImportantDevicesIcon/>
-          HP
-        </MenuItem>
-      </StyledMenu>
+        <Link href = "/Desktop/MSI_Desktop">
+        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> MSI</MenuItem>
+        </Link>
+        <Link href = "/Desktop/ASUS_Desktop">
+        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> ASUS</MenuItem>
+        </Link>
+        <Link href = "/Desktop/Dell_Desktop">
+        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> Dell</MenuItem>
+        </Link>
+        <Link href = "/Desktop/Lenovo_Desktop">
+        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> Lenovo</MenuItem>
+        </Link>
+        <Link href = "/Desktop/iMac">
+        <MenuItem onClick={handleClose}> <DesktopMacIcon style={{marginRight: 25}}  /> Mac</MenuItem>
+        </Link>
+      </Menu>
     </div>
   );
 }
